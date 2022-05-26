f218809074fa9ff424f0cc1e95135399d0d08ddd
Adrian Wojtaszek <aditqo@Adrians-MacBook-Pro.local>
2022-05-26 16:48:35 +0100

    used gofmt -s on the files

--- mutils/slice_contains.go
+++ mutils/slice_contains.go
@@ -1,8 +1,8 @@
 package mutils
 
 import (
-        "reflect"
-        "strings"
+	"reflect"
+	"strings"
 )
 
 /*
@@ -13,45 +13,45 @@ Allowed types: string | int | float64
 */
 func SliceContains(input interface{}, content interface{}) bool {
 
-        inType := reflect.TypeOf(input).String()
-        conType := reflect.TypeOf(content).String()
+	inType := reflect.TypeOf(input).String()
+	conType := reflect.TypeOf(content).String()
 
-        // panic if types are not matching
-        if !strings.Contains(inType, conType) {
-                panic("type missmatch in 'SliceContains', expected []" + conType + ", got " + inType)
-        }
+	// panic if types are not matching
+	if !strings.Contains(inType, conType) {
+		panic("type missmatch in 'SliceContains', expected []" + conType + ", got " + inType)
+	}
 
-        switch t := input.(type) {
+	switch t := input.(type) {
 
-        // slice of strings
-        case []string:
-                for _, value := range t {
-                        if value == content.(string) {
-                                return true
-                        }
-                }
+	// slice of strings
+	case []string:
+		for _, value := range t {
+			if value == content.(string) {
+				return true
+			}
+		}
 
-        // slice of ints
-        case []int:
-                for _, value := range t {
-                        if value == content.(int) {
-                                return true
-                        }
-                }
+	// slice of ints
+	case []int:
+		for _, value := range t {
+			if value == content.(int) {
+				return true
+			}
+		}
 
-        // slice of floats
-        case []float64:
-                for _, value := range t {
-                        if value == content.(float64) {
-                                return true
-                        }
-                }
+	// slice of floats
+	case []float64:
+		for _, value := range t {
+			if value == content.(float64) {
+				return true
+			}
+		}
 
-        default:
-                return false
+	default:
+		return false
 
-        }
+	}
 
-        return false
+	return false
 
 }
--- mutils/slice_contains_test.go
+++ mutils/slice_contains_test.go
@@ -4,39 +4,39 @@ import "testing"
 
 func TestSliceContains(t *testing.T) {
 
-        // preparing slices of different types
-        stringSlices := []string{"hello", "world"}
-        intSlices := []int{0, 1, 2, 3}
-        floatSlices := []float64{1.0, 2.0, 3.5}
+	// preparing slices of different types
+	stringSlices := []string{"hello", "world"}
+	intSlices := []int{0, 1, 2, 3}
+	floatSlices := []float64{1.0, 2.0, 3.5}
 
-        // positive contains string
-        if res := SliceContains(stringSlices, "world"); res != true {
-                t.Fatalf("the result of test should be true, instead of false")
-        }
+	// positive contains string
+	if res := SliceContains(stringSlices, "world"); res != true {
+		t.Fatalf("the result of test should be true, instead of false")
+	}
 
-        // negative contains string
-        if res := SliceContains(stringSlices, "sea"); res != false {
-                t.Fatalf("the result of test should be false, instead of true")
-        }
+	// negative contains string
+	if res := SliceContains(stringSlices, "sea"); res != false {
+		t.Fatalf("the result of test should be false, instead of true")
+	}
 
-        // positive contains int
-        if res := SliceContains(intSlices, 1); res != true {
-                t.Fatalf("the result of test should be true, instead of false")
-        }
+	// positive contains int
+	if res := SliceContains(intSlices, 1); res != true {
+		t.Fatalf("the result of test should be true, instead of false")
+	}
 
-        // negative contains int
-        if res := SliceContains(intSlices, 5); res != false {
-                t.Fatalf("the result of test should be false, instead of true")
-        }
+	// negative contains int
+	if res := SliceContains(intSlices, 5); res != false {
+		t.Fatalf("the result of test should be false, instead of true")
+	}
 
-        // positive contains float64
-        if res := SliceContains(floatSlices, 1.0); res != true {
-                t.Fatalf("the result of test should be true, instead of false")
-        }
+	// positive contains float64
+	if res := SliceContains(floatSlices, 1.0); res != true {
+		t.Fatalf("the result of test should be true, instead of false")
+	}
 
-        // negative contains float64
-        if res := SliceContains(floatSlices, 5.0); res != false {
-                t.Fatalf("the result of test should be false, instead of true")
-        }
+	// negative contains float64
+	if res := SliceContains(floatSlices, 5.0); res != false {
+		t.Fatalf("the result of test should be false, instead of true")
+	}
 
 }
